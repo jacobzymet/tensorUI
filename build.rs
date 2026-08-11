993f02e0f26e2958dfa920c40273704914266ec1
JZ <jacobzymet@proton.me>
2026-08-11 08:31:51 -0400

    use icon

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("cargo:rerun-if-changed=assets/app.ico");
+    println!("cargo:rerun-if-changed=assets/browser-favicon.png");
+
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("assets/app.ico");
+        res.set("ProductName", "tensorUI");
+        res.set("FileDescription", "tensorUI");
+        res.compile()
+            .expect("failed to compile Windows resources for app icon");
+    }
+}
